0ff6b79966905151944a1552d304028e29664669
Oscar <oscarjr01.13@gmail.com>
2021-09-22 15:50:15 -0400

    second

--- isbn10.py
+++ isbn10.py
@@ -1,13 +1,15 @@
 import unittest
 
-from main import isbn10_checker
+from main import isbn10_checker, isbn13_checker
 
 
 class MyTestCase(unittest.TestCase):
-    def test_isbn(self):
+    def test_isbn10(self):
         test = isbn10_checker("0716703440")
-        self.assertEqual(0, test)  # add assertion here
+        self.assertEqual(True, test)  # add assertion here
 
-
+    def test_isbn13(self):
+        test = isbn13_checker("9780716703440")
+        self.assertEqual(True, test)
 if __name__ == '__main__':
     unittest.main()
--- main.py
+++ main.py
@@ -2,10 +2,19 @@ def isbn10_checker(number):
     count = 1
     total = 0
     for i in range(len(number) - 1 ,-1,-1):
-        total = int(number[i])*count
-        print(f"{number[i]} * {count}")
+        total += int(number[i])*count
         count += 1
+    return total % 11 == 0
 
+def isbn13_checker(number):
+    count = 0
+    total = 0
+    for i in range(len(number) - 1 ,-1,-1):
+        if count == 1:
+            count = 3
+        else:
+            count = 1
+        total += int(number[i]) * count
 
-    return total % 11
-
+    print(total)
+    return total % 10 == 0
