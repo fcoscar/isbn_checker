339f2a6a4cf7491483fa18754a01f07b40fcfb3d
Oscar <oscarjr01.13@gmail.com>
2021-09-22 16:01:45 -0400

    third

--- main.py
+++ main.py
@@ -1,20 +1,28 @@
+def valid_isbn(number):
+    if len(number) != 10 or len(number) !=13:
+        return False
+
 def isbn10_checker(number):
     count = 1
     total = 0
-    for i in range(len(number) - 1 ,-1,-1):
-        total += int(number[i])*count
-        count += 1
-    return total % 11 == 0
+    if valid_isbn(number):
+        for i in range(len(number) - 1 ,-1,-1):
+            total += int(number[i])*count
+            count += 1
+        return total % 11 == 0
+    else:
+        return False
 
 def isbn13_checker(number):
     count = 0
     total = 0
-    for i in range(len(number) - 1 ,-1,-1):
-        if count == 1:
-            count = 3
-        else:
-            count = 1
-        total += int(number[i]) * count
-
-    print(total)
-    return total % 10 == 0
+    if valid_isbn(number):
+        for i in range(len(number) - 1 ,-1,-1):
+            if count == 1:
+                count = 3
+            else:
+                count = 1
+            total += int(number[i]) * count
+        return total % 10 == 0
+    else:
+        return False
